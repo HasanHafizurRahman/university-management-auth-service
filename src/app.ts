350aae596cbf3cf3eaa2e0709e6c05c3acbbfa5e
Hasan Hafizur Rahman <hasanshanto922@gmail.com>
2023-05-30 00:35:05 +0600

    database express setup

---
+++ src/app.ts
@@ -0,0 +1,11 @@
+import express, { Application } from "express";
+import cors from "cors";
+const app: Application = express();
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+export default app;
